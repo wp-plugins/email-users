41e82e238d50f83f1ff9347214a07d13a83b4647
mpwalsh8 <mpwalsh8@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-22 17:03:26 +0000

    Debug code left in place!  Oops.
    git-svn-id: https://plugins.svn.wordpress.org/email-users/trunk@562516 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- email-users.php
+++ email-users.php
@@ -43,7 +43,7 @@ define( 'MAILUSERS_ACCEPT_NOTIFICATION_USER_META', 'email_users_accept_notificat
 define( 'MAILUSERS_ACCEPT_MASS_EMAIL_USER_META', 'email_users_accept_mass_emails' );
 
 // Debug
-define( 'MAILUSERS_DEBUG', false);
+define( 'MAILUSERS_DEBUG', true);
 
 /**
  * Initialise the internationalisation domain
@@ -846,7 +846,7 @@ function mailusers_send_mail($recipients = array(), $subject = '', $message = ''
 		if (mailusers_is_valid_email($recipients[0]->user_email)) {
 			$headers .= "To: \"" . $recipients[0]->display_name . "\" <" . $recipients[0]->user_email . ">\n";
 			$headers .= "Cc: " . $sender_email . "\n\n";
-			@mailusers_preprint_r($sender_email, $subject, $mailtext, $headers);
+			@wp_mail($sender_email, $subject, $mailtext, $headers);
 			$num_sent++;
 		} else {
 			echo '<div class="error fade">The email address of the user you are trying to send mail to is not a valid email address format.</div>';
@@ -885,7 +885,7 @@ function mailusers_send_mail($recipients = array(), $subject = '', $message = ''
 				} else {
 					$newheaders = $headers . "$bcc\n\n";
 				}
-				@mailusers_preprint_r($sender_email, $subject, $mailtext, $newheaders);
+				@wp_mail($sender_email, $subject, $mailtext, $newheaders);
 				$count = 0;
 				$bcc = '';
 			}
@@ -909,7 +909,7 @@ function mailusers_send_mail($recipients = array(), $subject = '', $message = ''
 			$num_sent++;
 		}
 		$newheaders = $headers . "$bcc\n\n";
-		@mailusers_preprint_r($sender_email, $subject, $mailtext, $newheaders);
+		@wp_mail($sender_email, $subject, $mailtext, $newheaders);
 	}
 
 	return $num_sent;
