2064e28326090abb81b394fb4dec9a3672dc4089
mpwalsh8 <mpwalsh8@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-10 14:39:48 +0000

    Prep for 4.3.8 release.
    git-svn-id: https://plugins.svn.wordpress.org/email-users/trunk@570105 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- email-users.php
+++ email-users.php
@@ -2,7 +2,7 @@
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
 /*
 Plugin Name: Email Users
-Version: 4.3.7
+Version: 4.3.8
 Plugin URI: http://www.marvinlabs.com/products/wordpress-addons/email-users/
 Description: Allows the site editors to send an e-mail to the blog users. Credits to <a href="http://www.catalinionescu.com">Catalin Ionescu</a> who gave me some ideas for the plugin and has made a similar plugin. Bug reports and corrections by Cyril Crua, Pokey and Mike Walsh.
 Author: MarvinLabs & Mike Walsh
@@ -27,7 +27,7 @@ Author URI: http://www.marvinlabs.com
 */
 
 // Version of the plugin
-define( 'MAILUSERS_CURRENT_VERSION', '4.3.7' );
+define( 'MAILUSERS_CURRENT_VERSION', '4.3.8' );
 
 // i18n plugin domain
 define( 'MAILUSERS_I18N_DOMAIN', 'email-users' );
@@ -826,7 +826,6 @@ function mailusers_get_roles( $exclude_id='', $meta_filter = '') {
  * $meta_filter can be '', MAILUSERS_ACCEPT_NOTIFICATION_USER_META, or MAILUSERS_ACCEPT_MASS_EMAIL_USER_META
  */
 function mailusers_get_recipients_from_ids( $ids, $exclude_id='', $meta_filter = '') {
-
     return mailusers_get_users($exclude_id, $meta_filter, array('include' => $ids)) ;
 }
 
