91bc494b87c8ce0ae4b41c1c9698d6823df2b780
mpwalsh8 <mpwalsh8@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-20 00:09:42 +0000

    Prep for 4.3.15 release.
    git-svn-id: https://plugins.svn.wordpress.org/email-users/trunk@627430 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- email-users.php
+++ email-users.php
@@ -2,7 +2,7 @@
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
 /*
 Plugin Name: Email Users
-Version: 4.3.15-beta-4
+Version: 4.3.15-beta-5
 Plugin URI: http://www.marvinlabs.com/products/wordpress-addons/email-users/
 Description: Allows the site editors to send an e-mail to the blog users. Credits to <a href="http://www.catalinionescu.com">Catalin Ionescu</a> who gave me some ideas for the plugin and has made a similar plugin. Bug reports and corrections by Cyril Crua, Pokey and Mike Walsh.
 Author: MarvinLabs & Mike Walsh
